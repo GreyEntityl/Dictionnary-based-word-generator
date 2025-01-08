cdd4b1cc77922fa89db507ce4d4ae4a517dbf47b
Sentyl._. <145197125+GreyEntityl@users.noreply.github.com>
2025-01-08 15:36:03 +0100

    dictionnary.parser.py
    Update

--- dictionnary.parser.py
+++ dictionnary.parser.py
@@ -1,51 +1,62 @@
 import json
 import os
 
-def save(data):
-    with open("word.data.json", "w", encoding="utf-8") as f:
-        json.dump(data, f)
-def load():
-    with open("word.data.json", "r", encoding="utf-8") as f:
-        return json.load(f)
+class Parser:
+    def __init__(self,language="en",data={},DICT="dico.en.txt"):
+        self.LANGUAGE=language
+        self.DATA=data
+        self.DICT=DICT
+    def getData(self):
+        return self.DATA[self.LANGUAGE]
+    def save(self,data=None):
+        if data==None:
+            data=self.DATA
+        with open("word.data.json", "w", encoding="utf-8") as f:
+            json.dump(data, f)
+    def load(self):
+        with open("word.data.json", "r", encoding="utf-8") as f:
+            return json.load(f)
+    def loadData(self):
+        self.DATA=self.load()
+    def reload(self,data=None):
+        self.save(data)
+        self.loadData()
+    def KeyErrorHandler(self,key,data):
+        try:
+            data[key]
+            return False
+        except KeyError:
+            return True
 
-def KeyErrorHandler(key,data):
-    try:
-        data[key]
-        return False
-    except KeyError:
-        return True
-save({"en":{}})
+
+parser=Parser()
+parser.reload({"en":{}})
 n=0
-LANGUAGE="en"
-DCT="dico.en.txt"
-print(load())
-DATA=load()
-DATA[LANGUAGE].update(start={"count":0})
-save(DATA)
+print(parser.DATA)
+parser.reload(parser.getData().update(start={"count":0}))
 l=0
-for line in open(DCT,"r",encoding="utf-8").read()[1:].splitlines():
+for line in open(parser.DICT,"r",encoding="utf-8").read()[1:].splitlines():
     i=0
-    DATA=load()
-    if KeyErrorHandler(line[0],DATA[LANGUAGE]["start"]):
-        DATA[LANGUAGE]["start"].update({line[0]:0})
-    DATA[LANGUAGE]["start"][line[0]]+=1
-    DATA[LANGUAGE]["start"]["count"]+=1
+    parser.reload()
+    if parser.KeyErrorHandler(line[0],parser.getData()["start"]):
+        parser.getData()["start"].update({line[0]:0})
+    parser.getData()["start"][line[0]]+=1
+    parser.getData()["start"]["count"]+=1
     l+=1
     for ch in line:
-        if KeyErrorHandler(ch,DATA[LANGUAGE]):
-            DATA[LANGUAGE].update({ch:{"count":0}})
+        if parser.KeyErrorHandler(ch,parser.getData()):
+            parser.getData().update({ch:{"count":0}})
         if i>=len(line)-1:
-            DATA[LANGUAGE][ch]["count"]+=1
-            if KeyErrorHandler("end",DATA[LANGUAGE][ch]):
-                DATA[LANGUAGE][ch].update(end=0)
-            DATA[LANGUAGE][ch]["end"]+=1
+            parser.getData()[ch]["count"]+=1
+            if parser.KeyErrorHandler("end",parser.getData()[ch]):
+                parser.getData()[ch].update(end=0)
+            parser.getData()[ch]["end"]+=1
             i+=1
             continue
-        if KeyErrorHandler(line[i+1],DATA[LANGUAGE][ch]):
-            DATA[LANGUAGE][ch].update({line[i+1]:0})
-        DATA[LANGUAGE][ch]["count"]+=1
-        DATA[LANGUAGE][ch][line[i+1]]+=1
+        if parser.KeyErrorHandler(line[i+1],parser.getData()[ch]):
+            parser.getData()[ch].update({line[i+1]:0})
+        parser.getData()[ch]["count"]+=1
+        parser.getData()[ch][line[i+1]]+=1
         i+=1
-    
-    save(DATA)
-print(load())
+
+print(parser.DATA)
